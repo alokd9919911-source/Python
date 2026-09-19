316cd9214627eede1de20002c9f0c51826b97ee8
alokd9919911-source <alokd9919911@gmail.com>
2026-09-19 17:00:39 +0530

    Operators

---
+++ 08_Operators.py
@@ -0,0 +1,27 @@
+# Arithmatic operators 
+a = 4
+b = 6
+c = a+b
+print(c)
+
+# Assignment Operators
+a = 4-2  # Assign 4-2 in a 
+b = 6
+
+print(a)
+
+b+=3  #Increment the value of b by 3 and then assign it to b 
+
+
+print(b)
+
+#Comparision operators 
+d = 5>4  #Comparision operators only for only give a boolean true or false 
+
+print(d)
+
+# Logical operators 
+
+e =  True or False
+
+print(e)
