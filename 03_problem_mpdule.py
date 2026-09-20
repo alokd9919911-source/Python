e728b8c2eb70200fa6b48cc1388a6bbefc1a017d
alokd9919911-source <alokd9919911@gmail.com>
2026-09-20 22:17:53 +0530

    module

--- 03_problem_mpdule.py
+++ 03_problem_mpdule.py
@@ -1,8 +1,14 @@
 import pyttsx3
+
+
 engine = pyttsx3.init()
 
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
+
+
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
 engine.say(" Hello Alok dubey ")
+
+
 engine.runAndWait()
