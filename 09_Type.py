014cf647d6066dd21e938fc66cd9d0e06fa6ae51
alokd9919911-source <alokd9919911@gmail.com>
2026-09-21 19:23:20 +0530

    Types in python

---
+++ 09_Type.py
@@ -0,0 +1,16 @@
+
+a = 31
+t = type(a)  #type function output integer
+
+print(t)
+
+
+a = "31.5"
+b = float(a) # output float
+t = type(b)
+
+print(t)
+
+a = "55.5"
+t = type(a)  # output string
+print(t)
---
+++ 10_Input.py
@@ -0,0 +1,26 @@
+
+
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+
+print("Number a is:", a)
+
+print("Number b is:", b)
+# this is string and print input = output
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("Number a is:", a)
+
+print("Number b is:", b)
+
+print("Sum is", a+b)
+
+# "Alok"+"Alok"= AlokAlok  using integer and sum "2"+"3"=5
+ 
+
+
+
+
+
